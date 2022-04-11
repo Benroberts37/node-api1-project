1c17f96d2eacb8be8ad3a06af6e0c69dfb857931
Benroberts37 <benrobsemail@gmail.com>
2022-04-11 16:49:34 -0400

    starting proejct

--- api/server.js
+++ api/server.js
@@ -1,3 +1,10 @@
 // BUILD YOUR SERVER HERE
+const express = require('express')
+
+const server = express()
+
+server.use('*', (req, res) => {
+    res.status(404).json({ message: 'not found' })
+})
 
 module.exports = {}; // EXPORT YOUR SERVER instead of {}
